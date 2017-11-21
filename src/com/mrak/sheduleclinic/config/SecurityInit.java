b63ef2f9b44f985173544231b9f0c635e003434f
Denis Tarasov <deolands@gmail.com>
2017-11-21 20:39:15 +0300

    added Spring Sequrity(not full)

---
+++ src/com/mrak/sheduleclinic/config/SecurityInit.java
@@ -0,0 +1,6 @@
+package com.mrak.sheduleclinic.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityInit extends AbstractSecurityWebApplicationInitializer{
+}
---
+++ src/com/mrak/sheduleclinic/model/Users.java
@@ -0,0 +1,78 @@
+package com.mrak.sheduleclinic.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table
+public class Users {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column
+    private String name;
+
+    @Column
+    private int role_id;
+
+    @Column
+    private String role;
+
+    @Column
+    private String password;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getRole_id() {
+        return role_id;
+    }
+
+    public void setRole_id(int role_id) {
+        this.role_id = role_id;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    @Column
+
+    private String login;
+
+    public Users() {
+    }
+}
