79c5155406a70cc63ca8bb7600229821b86c49e1
Denis Tarasov <deolands@gmail.com>
2017-11-24 18:59:48 +0300

    added autorisation(with database)

--- src/com/mrak/sheduleclinic/config/SecurityInit.java
+++
@@ -1,6 +0,0 @@
-package com.mrak.sheduleclinic.config;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class SecurityInit extends AbstractSecurityWebApplicationInitializer{
-}
